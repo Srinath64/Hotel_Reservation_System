316f88ec95313873c78a8c4811f256b5d97256c5
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-01-06 14:41:16 +0530

    Added : UC1-Hotel class for Regular Customers

---
+++ src/test/java/com/hotelreservationsystem/HotelReservationTest.java
@@ -0,0 +1,18 @@
+package com.hotelreservationsystem;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HotelReservationTest {
+    @Test
+    public void givenHotelDetails_WhenValuesEnteredAreCorrect_ShouldReturnTrue()
+    {
+        HotelReservation hotelReservationService = new HotelReservation();
+        boolean isValidHotel1 = hotelReservationService.addHotel("Lakewood", 3, 110, 90);
+        Assertions.assertTrue(isValidHotel1);
+        boolean isValidHotel2 = hotelReservationService.addHotel("Bridgewood", 4, 160, 60);
+        Assertions.assertTrue(isValidHotel2);
+        boolean isValidHotel3 = hotelReservationService.addHotel("Ridgewood", 5, 220, 150);
+        Assertions.assertTrue(isValidHotel3);
+    }
+}
