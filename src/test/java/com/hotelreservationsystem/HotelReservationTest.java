5bc0ff1e750b79b4cebd04589c054e4886c501f1
Srinath S <srinath@Srinaths-MacBook-Air.local>
2022-01-07 13:58:23 +0530

    Added : UC5

--- src/test/java/com/hotelreservationsystem/HotelReservationTest.java
+++ src/test/java/com/hotelreservationsystem/HotelReservationTest.java
@@ -6,35 +6,13 @@ import com.hotelreservationsystem.service.HotelReservationInterface;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.time.LocalDate;
-import java.time.Month;
 
 public class HotelReservationTest {
     @Test
-    public void givenHotelList_WhenAdded_shouldReturnProperHotelWeekdayRegularCustomerCost(){
+    public void givenHotelList_WhenAdded_shouldReturnProperHotelRating(){
         HotelReservationInterface hotelReservation = new HotelReservation();
-        hotelReservation.addHotel("Bridgewood", 4, 150, 50);
-        int hotelRegularCustomerCost = (int) hotelReservation.getHotelList().get(0).getWeekdayRegularCustomerCost();
-        Assertions.assertEquals(150, hotelRegularCustomerCost);
-    }
-
-    @Test
-    public void givenHotelList_WhenAdded_shouldReturnProperHotelWeekendRegularCustomerCost(){
-        HotelReservationInterface hotelReservation = new HotelReservation();
-        hotelReservation.addHotel("Bridgewood", 4, 150, 50);
-        int hotelRegularCustomerCost = (int) hotelReservation.getHotelList().get(0).getWeekendRegularCustomerCost();
-        Assertions.assertEquals(50, hotelRegularCustomerCost);
-    }
-
-    @Test
-    public void givenHotelDetails_shouldReturnCheapestHotel(){
-
-        HotelReservation hotelReservation = new HotelReservation();
-        hotelReservation.addHotel("Lakewood", 3, 110, 90);
-        hotelReservation.addHotel("Bridgewood", 4, 160, 50);
-        LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);
-        LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 12);
-        String hotelName = hotelReservation.getCheapestHotel(startDate, endDate);
-        Assertions.assertEquals("Lakewood", hotelName);
+        hotelReservation.addHotel("Ridgewood", 5, 220, 150);
+        int hotelRating = hotelReservation.getHotelList().get(0).getRating();
+        Assertions.assertEquals(5, hotelRating);
     }
 }
